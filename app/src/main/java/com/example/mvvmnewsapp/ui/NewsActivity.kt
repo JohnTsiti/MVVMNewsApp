dc63b590a2b8f1d546eb9996b13515b1076fec68
tsitiridis <tsitiridisgiannis@gmail.com>
2022-06-28 17:12:47 +0300

    main app and navigation component setup

---
+++ app/src/main/java/com/example/mvvmnewsapp/ui/NewsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mvvmnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mvvmnewsapp.R
+import com.example.mvvmnewsapp.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNavigationView.setupWithNavController(Navigation
+            .findNavController(this,R.id.newsNavHostFragment))
+
+    }
+}
